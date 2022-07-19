fb99f6406a5cbca20df178ba85504a7fb20e5088
druiz912 <druiz264@gmail.com>
2022-07-19 18:23:42 +0000

    add more's indexs

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our", "your", "weedle", "biggie"];
+  var adj = ["great", "big", "others", "pipe"];
+  var noun = ["jogger", "racoon", "kali", "malvin"];
+  var dom = [".com", ".es", ".net", ".org"];
+
+  let sentence =
+    pronoun[numAle(pronoun.length)] +
+    adj[numAle(adj.length)] +
+    noun[numAle(noun.length)] +
+    dom[numAle(dom.length)];
+
+  let excuse = document.querySelector("#p-domain");
+  excuse.innerHTML = sentence;
 };
+
+function numAle(length) {
+  let number = Math.floor(Math.random() * length);
+  return number;
+}
