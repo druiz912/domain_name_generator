a451054daecf95ebcd4cf78d14a7de1aa69c0e4e
druiz912 <druiz264@gmail.com>
2022-08-08 19:08:04 +0000

    fix error in app.js to show the whole array

--- src/app.js
+++ src/app.js
@@ -1,27 +1,22 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  var pronoun = ["the", "our", "your", "weedle", "biggie"];
-  var adj = ["great", "big", "others", "pipe"];
-  var noun = ["jogger", "racoon", "kali", "malvin"];
-  var dom = [".com", ".es", ".net", ".org"];
-
-  let sentence =
-    pronoun[numAle(pronoun.length)] +
-    adj[numAle(adj.length)] +
-    noun[numAle(noun.length)] +
-    dom[numAle(dom.length)];
-
-  let excuse = document.querySelector("#p-domain");
-  excuse.innerHTML = sentence;
+  //write your code here
+  console.log("Hola desde la consola!");
+  document.querySelector("#dru").innerHTML = name;
 };
 
-function numAle(length) {
-  let number = Math.floor(Math.random() * length);
-  return number;
+let adjetivo = ["little", "great", "big", "smart", "natural", "wild", "bored"];
+let sustantivo = ["horse", "name", "cat", "facts", "true", "mouls"];
+let extns = [".com", ".es", ".net", ".io", ".org"];
+let name = "";
+for (let k = 0; k < extns.length; k++) {
+  for (let i = 0; i < adjetivo.length; i++) {
+    for (let j = 0; j < sustantivo.length; j++) {
+      name += adjetivo[i] + sustantivo[j] + extns[k] + "<br>"; //salto linea
+    }
+  }
 }
